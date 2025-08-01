1b36f734b0c60403401f8e07e5b89795c28a5995
Arpitverma2006 <vermaajay6799013@gmail.com>
2025-08-01 18:38:13 +0530

    working

--- api/index.py
+++ api/index.py
@@ -1,24 +1,14 @@
 import os
 import sys
-from fastapi import FastAPI
 from mangum import Mangum
-
-# Add Django project to sys.path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omts.settings")
-
-import django
 from django.core.asgi import get_asgi_application
 
+# Setup Django settings
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OnlineTestingSystem.settings")
+
+import django
 django.setup()
 
-# Wrap Django ASGI inside FastAPI
-django_app = get_asgi_application()
-app = FastAPI()
-
-@app.middleware("http")
-async def django_adapter(request, call_next):
-    # Let Django ASGI handle it
-    return await django_app(request.scope, request.receive, request.send)
-
-handler = Mangum(app)  # Required by Vercel
+application = get_asgi_application()
+handler = Mangum(application)
